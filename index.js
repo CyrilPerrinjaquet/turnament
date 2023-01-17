efd92743e731cd524e285d8029ac4b12acd82732
Cyril <cyrilp.perso@gmail.com>
2023-01-17 16:49:47 +0100

    🎨 format(QO-14191) formated the code correctly
    formated the code with prettier for better lisibility

--- index.js
+++ index.js
@@ -4,51 +4,54 @@ const matchListElement = document.getElementById("matchsList");
 
 const matchList = [];
 
-
 function getDataFromForm(event) {
-    event.preventDefault();
-    const dataForm = new FormData(formElement);
-    const teamA = dataForm.get("teamA");
-    const teamB = dataForm.get("teamB");
-    const scoreTeamA = dataForm.get("scoreTeamA");
-    const scoreTeamB = dataForm.get("scoreTeamB");
+  event.preventDefault();
+  const dataForm = new FormData(formElement);
+  const teamA = dataForm.get("teamA");
+  const teamB = dataForm.get("teamB");
+  const scoreTeamA = dataForm.get("scoreTeamA");
+  const scoreTeamB = dataForm.get("scoreTeamB");
 
-    addMatchToMatchList({ teamA, teamB, scoreTeamA, scoreTeamB });
+  addMatchToMatchList({ teamA, teamB, scoreTeamA, scoreTeamB });
 }
 
-function isMatchValid({ teamA, teamB, scoreTeamA, scoreTeamB }) {
-    return !(teamA.trim() === "" || teamB.trim() === "" || teamA.toLowerCase() === teamB.toLowerCase());
+function isMatchValid({ teamA, teamB }) {
+  return !(
+    teamA.trim() === "" ||
+    teamB.trim() === "" ||
+    teamA.toLowerCase() === teamB.toLowerCase()
+  );
 }
 
 function addMatchToMatchList({ teamA, teamB, scoreTeamA, scoreTeamB }) {
-    if (isMatchValid({ teamA, teamB, scoreTeamA, scoreTeamB })) {
-        const winner = getWinner({ teamA, teamB, scoreTeamA, scoreTeamB });
-        matchList.push({ teamA, teamB, winner });
-        updateMatchListElement();
-    } else {
-        alert("Match isn't valid");
-    }
-
+  if (isMatchValid({ teamA, teamB, scoreTeamA, scoreTeamB })) {
+    const winner = getWinner({ teamA, teamB, scoreTeamA, scoreTeamB });
+    matchList.push({ teamA, teamB, winner });
+    updateMatchListElement();
+  } else {
+    alert("Match isn't valid");
+  }
 }
 
 function getWinner({ teamA, teamB, scoreTeamA, scoreTeamB }) {
-    let winner;
-    if (scoreTeamA > scoreTeamB) {
-        winner = teamA;
-    } else if (scoreTeamB > scoreTeamA) {
-        winner = teamB;
-    }
-    return winner;
+  let winner;
+  if (scoreTeamA > scoreTeamB) {
+    winner = teamA;
+  } else if (scoreTeamB > scoreTeamA) {
+    winner = teamB;
+  }
+  return winner;
 }
 
 function updateMatchListElement() {
-    const lastElementFromArray = matchList[matchList.length - 1];
+  const { teamA, teamB, winner } = matchList[matchList.length - 1];
 
-    const newMatchItem = document.createElement("li");
-    const itemText = document.createTextNode(`${lastElementFromArray.teamA} VS ${lastElementFromArray.teamB} => ${lastElementFromArray.winner ?? "No winner"}`);
-    newMatchItem.appendChild(itemText);
-    matchListElement.appendChild(newMatchItem);
+  const newMatchItem = document.createElement("li");
+  const itemText = document.createTextNode(
+    `${teamA} VS ${teamB} => ${winner ?? "No winner"}`
+  );
+  newMatchItem.appendChild(itemText);
+  matchListElement.appendChild(newMatchItem);
 }
-
 
 formElement.onsubmit = getDataFromForm;
